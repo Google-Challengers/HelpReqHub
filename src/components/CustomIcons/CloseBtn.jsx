31ea6f9e1308ab8c6a1a4a0a0ce0f4471a01a22d
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-01-17 16:44:05 +0530

    created 2 custom icons btns

---
+++ src/components/CustomIcons/CloseBtn.jsx
@@ -0,0 +1,27 @@
+const CloseBtn = ({ clickEvent }) => {
+  return (
+    <>
+      <button
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        onClick={clickEvent}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="w-5 h-5"
+        >
+          <line x1="18" y1="6" x2="6" y2="18"></line>
+          <line x1="6" y1="6" x2="18" y2="18"></line>
+        </svg>
+      </button>
+    </>
+  );
+};
+
+export default CloseBtn;
---
+++ src/components/CustomIcons/Hamburger.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+const Hamburger = ({ clickEvent }) => {
+  return (
+    <>
+      <button
+        type="button"
+        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        onClick={() => {
+          clickEvent();
+        }}
+      >
+        <svg
+          className="w-5 h-5"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 17 14"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M1 1h15M1 7h15M1 13h15"
+          />
+        </svg>
+      </button>
+    </>
+  );
+};
+
+export default Hamburger;
