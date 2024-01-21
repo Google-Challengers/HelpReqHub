a00bbf7779408f65b7c1d15ceba7417d7dbb53a2
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-01-21 18:17:39 +0530

    created the global request model

---
+++ src/lib/models/globalRequest/request.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Types.ObjectId, required: true, ref: "user" },
+    title: {
+      type: String,
+      required: [true, "Title is required"],
+      trim: true,
+      lowercase: true,
+    },
+    desc: {
+      type: String,
+      required: [true, "Description is required"],
+      trim: true,
+      lowercase: true,
+      lowercase: true,
+    },
+    time: {
+      type: String,
+      required: [true, "Time is required"],
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "complete"],
+      default: "pending",
+      lowercase: true,
+      required: [true, "Status can be one of [COMPLETE, PENDING]"],
+      trim: true,
+    },
+    requestHandlers: {
+      type: [{ type: mongoose.Types.ObjectId, ref: "user" }],
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+export const RequestModel =
+  mongoose.models["request"] || new mongoose.model("request", requestSchema);
