9a972cdd29ef3dd435f81431718e462bea2136d6
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-02-23 09:50:59 +0530

    created marker for requester and helper

--- src/components/Location/GMap.jsx
+++ src/components/Location/GMap.jsx
@@ -7,18 +7,59 @@ const center = {
   lng: 76.7794,
 };
 
-const Marker = ({ text }) => <div className="text-red-600">{text}</div>;
-
-const GMap = () => {
+const HelpersMarker = () => {
   return (
     <>
+      <div className="w-6 h-6 flex flex-col items-center justify-center bg-blue-700 rounded-full">
+        <div className="w-3 h-3 bg-white text-black animate-pulse rounded-full transition-all shadow-md"></div>
+      </div>
+    </>
+  );
+};
+
+const RequestersMarker = () => {
+  return (
+    <>
+      <div className="w-6 h-6 flex flex-col items-center justify-center bg-red-700 rounded-full">
+        <div className="w-3 h-3 bg-white text-black animate-ping rounded-full transition-all shadow-md"></div>
+      </div>
+    </>
+  );
+};
+
+const GMap = () => {
+  const userRole = "receiver";
+
+  return (
+    <>
+      <div className="flex flex-col gap-2 my-2 w-full p-2">
+        <h3 className="font-black text-black underline">[*] Markers</h3>
+        <div className="flex flex-row items-center gap-1">
+          <HelpersMarker />
+          <span className="mx-1 text-xs font-light text-black">Helpers</span>
+        </div>
+        <div className="flex flex-row items-center gap-1">
+          <RequestersMarker />
+          <span className="mx-1 text-xs font-light text-black">Requesters</span>
+        </div>
+      </div>
       <div className="w-full min-h-screen p-2">
         <GoogleMapReact
           bootstrapURLKeys={{ key: process.env.NEXT_PUBLIC_GOOGLE_MAP_API_KEY }}
           defaultCenter={center}
           defaultZoom={15}
+          center={center}
+          margin={[10, 10, 10, 10]}
+          options={{}}
+          onChange={(e) => {}}
+          onChildClick={(child) => {}}
         >
-          <Marker lat={59.955413} lng={30.337844} text="My Marker" />
+          {/* Based on user role show the marker on the map */}
+          {userRole.toLowerCase() == "receiver" ? (
+            <HelpersMarker lat={center.lat} lng={center.lng} />
+          ) : (
+            <RequestersMarker lat={center.lat} lng={center.lng} />
+          )}
         </GoogleMapReact>
       </div>
     </>
